6efa62e0a43d0af894a7843989a6b2ff3dd475e1
bigbadabum6 <dolgkozly@gmail.com>
2022-06-24 16:43:38 +0300

    Map commit

--- js/intro.js
+++ js/intro.js
@@ -4,3 +4,23 @@ const arr = [
   [1, "first"],
   [3, "third"],
 ];
+
+const map = new Map();
+
+map.set(1, "first").set(3, "third");
+for (let key of map.keys()) {
+  console.log("key :>> ", key);
+}
+
+for (let value of map.values()) {
+  console.log("value :>> ", value);
+}
+
+console.log("map.size :>> ", map.size);
+
+map.set(2, "second");
+map.delete(2);
+console.log(
+  "map.has(1) :>> ",
+  map.has(1)
+);
